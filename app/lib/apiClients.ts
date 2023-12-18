36c85b9bf0ea328381bb6d58ddeecab5058cf4e8
kanaria0710 <gachazam@icloud.com>
2023-12-19 00:38:08 +0900

    fix/baseUrl

--- app/lib/apiClients.ts
+++ app/lib/apiClients.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const apiClients = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    // baseURL: 'http://localhost:8080/api',
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
     headers: {
         'Content-Type': 'application/json',
     },
